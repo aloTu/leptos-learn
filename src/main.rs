2d1911c8f0d5bb8dd60ccf43f66b836060c43390
tuanlong <tuanlong@kingsoft.com>
2024-07-25 11:11:35 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use leptos::*;
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                // on stable, this is set_count.set(3);
+                set_count(count()+1);
+            }
+        >
+            "Click me: "
+            // on stable, this is move || count.get();
+            {move || count()}
+        </button>
+    }
+}
+
+pub fn main() {
+    // _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    mount_to_body(|| {
+        view! { <App/> }
+    });
+}
